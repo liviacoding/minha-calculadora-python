761aee2507434b4870ec601401f6ffa4b3992ddd
Livia <liviasoares.tech@gmail.com>
2026-03-15 17:11:12 -0300

    Minha calculadora Funcional

--- App_Calculadora.py
+++ App_Calculadora.py
@@ -3,6 +3,27 @@ print("------ CALCULADORA DA LIVIA------")
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input('Digite o segundo número: '))
 
-soma = n1 + n2
+print("\nEscolhaa a operação:")
+print("+ para somar")
+print("* para multiplicar")
+print("/ para dividir")
+print("- para Subtrair")
 
-print(f'O resultado da soma é: {soma}')
+operacao = input("\nQual Operação deseja fazer?: ")
+
+if operacao == "+":
+    resultado = n1 + n2
+    print("\nO resultado da operação é: {}".format(resultado))
+elif operacao == "-":
+    resultado = n1 - n2
+    print("\nO resultado da operação é: {}".format(resultado))
+elif operacao == "*":
+    resultado = n1 * n2
+    print("\nO resultado da operação é: {}".format(resultado))
+elif operacao == "/":
+    resultado = n1 / n2
+    print("\nO resultado da operação é: {}".format(resultado))
+else:
+    print("\nERRO : Não é possivel dividir por ZERO!")
+    print("\nOperação inválida!")
+

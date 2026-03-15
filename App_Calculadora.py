880627b667ed80194b1496e77f28b6143b79f4e4
Livia <livasoares.tech@gmail.com>
2026-03-15 16:17:11 -0300

    Minha calculadora Funcional

---
+++ App_Calculadora.py
@@ -0,0 +1,8 @@
+print("------ CALCULADORA DA LIVIA------")
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input('Digite o segundo número: '))
+
+soma = n1 + n2
+
+print(f'O resultado da soma é: {soma}')

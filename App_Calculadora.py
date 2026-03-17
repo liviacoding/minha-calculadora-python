27a8be925635df126c6f53a0c8d7295a68d270e3
Livia <liviasoares.tech@gmail.com>
2026-03-17 16:20:16 -0300

    Calculadora atualizada com módulos

--- App_Calculadora.py
+++ App_Calculadora.py
@@ -1,29 +1,23 @@
+import funcao
+
 print("------ CALCULADORA DA LIVIA------")
 
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input('Digite o segundo número: '))
 
-print("\nEscolhaa a operação:")
-print("+ para somar")
-print("* para multiplicar")
-print("/ para dividir")
-print("- para Subtrair")
+print("\nEscolhaa a operação: + - * /")
+
 
 operacao = input("\nQual Operação deseja fazer?: ")
 
 if operacao == "+":
-    resultado = n1 + n2
-    print("\nO resultado da operação é: {}".format(resultado))
-elif operacao == "-":
-    resultado = n1 - n2
-    print("\nO resultado da operação é: {}".format(resultado))
-elif operacao == "*":
-    resultado = n1 * n2
-    print("\nO resultado da operação é: {}".format(resultado))
-elif operacao == "/":
-    resultado = n1 / n2
-    print("\nO resultado da operação é: {}".format(resultado))
-else:
-    print("\nERRO : Não é possivel dividir por ZERO!")
-    print("\nOperação inválida!")
+    print(f"\nO resultado da operação é: {funcao.soma(n1,n2)}")
 
+elif operacao == "-":
+     print(f"\nO resultado da operação é: {funcao.subtracao(n1,n2)}")
+
+elif operacao == "*":
+    print(f"\nO resultado da operação é: {funcao.multiplicacao(n1,n2)}")
+
+elif operacao == "/":
+    print(f"\nO resultado da operação é: {funcao.divisao(n1,n2)}")
